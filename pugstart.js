e45fab01559bb9e9e91b50d046a484cebee0ca22
Volodymyr Nerovnia <nerv@i.ua>
2024-02-27 23:06:43 +0200

    Initial commit

---
+++ pugstart.js
@@ -0,0 +1,24 @@
+import pug from "pug";
+import pugConfig from "./config/pug-config";
+
+console.log(pugConfig);
+
+/*
+// Compile the source code
+const compiledFunction = pug.compileFile("template.pug");
+
+// Render a set of data
+console.log(
+  compiledFunction({
+    name: "Timothy",
+  })
+);
+// "<p>Timothy's Pug source code!</p>"
+
+// Render another set of data
+console.log(
+  compiledFunction({
+    name: "Forbes",
+  })
+);
+*/
